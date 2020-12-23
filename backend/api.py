c1cafcc768ea6ff6ce8786895946b6332296d4c8
Pride <pkavumba@gmail.com>
2020-12-23 18:36:01 +0900

    Update api.py

--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
@@ -14,11 +14,26 @@ class Warrant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     warrant = db.Column(db.String, unique=True, nullable=False)
 
+def calculate(x):
+    """return quality score of the warrant"""
+    
+    return {'score': 1}
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def analyze():
-    return jsonify({'score': 1})
+    if request.method == 'GET':
+        warrant = request.args.get('warrant', default="", type=str) # get the warrant to be analyzed
+        score = calculate(warrant) # analyze the warrant and return the score
+        return jsonify(score)
+    elif request.method == 'POST':
+        payload = request.json
+        warrant = Warrant(payload.get('warrant')) # create the warrant db object
+        db.session.add(warrant) # stage the warrant for saving
+        db.session.commit() # save the warrant
+        return jsonify(warrant.to_dict()) # return the saved warrant
+    else:
+        return jsonify({'details': "welcome to the api"})
+    
 
 
 if __name__ == "__main__":
